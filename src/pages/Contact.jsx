c4dea3dbfb34e224b38db4bf1275d0bc384c07b1
CarlosErencia <a19carerecas@inspedralbes.cat>
2021-05-04 12:37:08 +0200

    update

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Mapa from '../components/Mapa'
+import FormContacto from '../components/FormContacto'
+
+const Contact = () => {
+return (
+    <div>
+        <FormContacto>
+        </FormContacto>
+
+        <Mapa>   
+        </Mapa>
+    </div>
+   
+)
+}
+export default Contact
