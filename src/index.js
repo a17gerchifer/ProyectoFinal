a4b2bc62a82f7ba0d5438ffa741c3d03b01bd5ba
a17gerchifer <[a17gerchifer@inspedralbes.cat]>
2021-05-11 12:37:51 +0200

    axios

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css'
 import * as serviceWorker from './serviceWorker';
 import {RoomProvider} from './context'
-import api from './api'
+
 ReactDOM.render(
   <React.StrictMode>
   <RoomProvider>
@@ -16,29 +16,3 @@ ReactDOM.render(
 
 serviceWorker.unregister();
 
-const IndexPage = () => {
-  // Create state variables
-  let [responseData, setResponseData] = React.useState('')
-  // fetches data
-  const fetchData = (e) => {
-      e.preventDefault()
-      api.getData()
-      .then((response)=>{
-          setResponseData(response.data)
-          console.log(response)
-      })
-      .catch((error) => {
-          console.log(error)
-      })
-  }
-  return (
-      <div>
-          <h1>{responseData.title}</h1>
-          <button onClick={(e) => fetchData(e)} type='button'>Click Me For Data</button>
-          {responseData.dates && responseData.dates.map(date => {
-              return <p>{date}</p>
-          })}
-      </div>
-  )
-}
-export default IndexPage
